4fbbf024346ad7e1c19c11b365aec8b6e17e47da
AdamFletcher_SAE <1021066@student.sae.edu.au>
2020-08-27 15:24:09 +1000

    Scripts  and Saturn
    I implimented a script to turn off and on the UI. I also implimented a script to rotate moons. Vanessa had to drop her wor so I built Saturn!

---
+++ Assets/Scripts/TurnOnUI.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnOnUI : MonoBehaviour
+{
+    public GameObject solarSystemUI;
+    private bool haveClicked;
+    // Start is called before the first frame update
+    void Start()
+    {
+        haveClicked = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {        
+        if(!haveClicked)
+        {
+            solarSystemUI.SetActive(true);
+            haveClicked = true;
+        }
+        else
+        {
+            solarSystemUI.SetActive(false);
+            haveClicked = false;
+        }
+    }
+}
